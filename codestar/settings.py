2a0519ba5544bba8654b6b40c8a597b360d56839
annie-mac <mcavoy.ana@gmail.com>
2025-06-28 10:14:40 +0100

    create env.py and link databases

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%@5vuu1g$dnsrk!_r7obgipnsj!*2052(g04ppxvfi=%*z&kte'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
