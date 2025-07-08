ddb3a064c0f0a90878f902f178ecdb1bd0a84511
annie-mac <mcavoy.ana@gmail.com>
2025-07-08 20:43:13 +0100

    customise admin with addition od summernote

--- codestar/settings.py
+++ codestar/settings.py
@@ -50,7 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-]
+    'django_summernote',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
