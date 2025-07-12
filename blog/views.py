fb854167ac9cd1a2ef765e46cf689b6239fb5121
annie-mac <mcavoy.ana@gmail.com>
2025-07-12 22:18:34 +0100

    fix the views eeror

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,15 @@
-from django.shortcuts import render,  get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
-
 
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    # queryset = Post.objects.all().order_by("-created_on")
-    # queryset = Post.objects.all().order_by("created_on")
-    # queryset = Post.objects.filter(author=2)
     template_name = "blog/index.html"
     paginate_by = 6
 
-    def post_detail(request, slug):
+
+def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
 
